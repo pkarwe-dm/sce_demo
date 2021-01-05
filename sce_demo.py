ddc776195b121e2832a9f1a5184cd7296d86cdc7
pkarwe-dm <67437941+pkarwe-dm@users.noreply.github.com>
2021-01-05 17:00:03 -0500

    Update sce_demo.py

--- sce_demo.py
+++ sce_demo.py
@@ -292,7 +292,7 @@ def run(gis_un, gis_pw, client_id, client_secret):
 
     # check to see if a layer already exists, if so update, else create
     # can alternatively save layer item ids to a store then reference
-    item = Item(gis, 'cc6c9d08bc814274a9e39439c0d1a21d')
+    item = Item(gis, 'fcd1dad0687741ae87bac9966fa727e1')
     lyr = item.layers[0]
     delete_before_days(lyr,30,'event_time') #delete old features
     logging.info('Updating existing layer {0} with {1} alerts'.format(item.id, len(alerts)))
